7000894203580c6e9a96bf31004bd8e4fad30cd6
Hunter Cleary <34077789+hncleary@users.noreply.github.com>
2019-09-24 22:35:18 -0500

    Started Homework 2

---
+++ Homework 2/Question1.py
@@ -0,0 +1,16 @@
+import numpy as np
+print "Numpy Version: " , (np.__version__)
+import matplotlib.pyplot as plt
+
+# Three Samples
+sample1 = [1, 3, 1]
+sample2 = [3, 2, -1]
+sample3 = [4, 1, -1]
+
+# initial weight (transpose)
+weight0 = [2, 1, -1]
+# 2 - x + y = 0
+
+
+
+
---
+++ Homework 2/Question2.py
@@ -0,0 +1,24 @@
+import numpy as np
+print "Numpy Version: " , (np.__version__)
+
+import csv as csv
+print "CSV Version: " , (csv.__version__)
+
+with open("airfoil_self_noise_test.csv") as csvNoiseTest:
+    reader = csv.DictReader(csvNoiseTest)
+    noiseTest_table = [row.split(",") for row in csvNoiseTest.read().replace("\r", "").split("\n")]
+    noiseTest_matrix = np.matrix(noiseTest_table)
+noiseTestRows = np.size(noiseTest_matrix, 0)
+noiseTestCols = np.size(noiseTest_matrix, 1)
+
+with open("airfoil_self_noise_train.csv") as csvNoiseTrain:
+    reader = csv.DictReader(csvNoiseTrain)
+    noiseTrain_table = [row.split(",") for row in csvNoiseTrain.read().replace("\r", "").split("\n")]
+    noiseTrain_matrix = np.matrix(noiseTrain_table)
+noiseTrainRows = np.size(noiseTrain_matrix, 0)
+noiseTrainCols = np.size(noiseTrain_matrix, 1)
+
+print noiseTestCols, noiseTestRows
+print noiseTrainCols, noiseTrainRows
+
+# print noiseTrain_matrix.item(2,0)
