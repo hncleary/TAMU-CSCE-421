f5d327fd5e6289c70b8a4c20af80fcfbaea2385d
Hunter Cleary <34077789+hncleary@users.noreply.github.com>
2019-09-29 12:49:07 -0500

    Starting Coding for HW 2 CSE 421

--- Homework 2/Question1.py
+++ Homework 2/Question1.py
@@ -3,14 +3,18 @@ print "Numpy Version: " , (np.__version__)
 import matplotlib.pyplot as plt
 
 # Three Samples
-sample1 = [1, 3, 1]
-sample2 = [3, 2, -1]
-sample3 = [4, 1, -1]
+sample1 = np.matrix([1, 3, 1])
+sample2 = np.matrix([3, 2, -1])
+sample3 = np.matrix([4, 1, -1])
 
 # initial weight (transpose)
-weight0 = [2, 1, -1]
+weight0 = np.matrix("2; 1; -1")
 # 2 - x + y = 0
 
+plt.scatter([1,3,4],[3,2,1])
+# plt.show()
 
-
-
+print sample1
+print sample2
+print sample3
+print weight0
